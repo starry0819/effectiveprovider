2647f0224924026c3e01d0f1159c879e79d2e41e
zhanghuanfa <997543429@qq.com>
2018-01-03 22:03:39 +0800

    init git

---
+++ src/main/java/com/zhanghuanfa/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.zhanghuanfa.service;
+
+import com.zhanghuanfa.model.po.User;
+
+/**
+ * @author zhanghuanfa
+ * @date 2018-01-02 20:22
+ */
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public User getUserById(Integer id) {
+        return null;
+    }
+}
